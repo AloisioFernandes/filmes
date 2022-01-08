51fc44c10127aff25ab4afd21bafaea4807920a0
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-01-08 20:32:46 -0300

    Componente Header.

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function Header() {
+  return (
+    <View>
+      <Text>HEADER TESTE</Text>
+    </View>
+  )
+}
+
+export default Header
