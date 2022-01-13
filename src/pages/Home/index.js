8061e86a262c3451ed129443cef21632128230c8
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-01-12 22:28:37 -0300

    Iniciando banner.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { ScrollView } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
-import { Container, SearchContainer, Input, SearchButton } from './styles'
+import { Container, SearchContainer, Input, Title, BannerButton, Banner } from './styles'
 import Header from '../../components/Header'
 
 function Home() {
@@ -17,9 +18,17 @@ function Home() {
           <Feather name="search" size={30} color="#FFF" />
         </SearchButton>
       </SearchContainer>
+
+      <ScrollView>
+        <Title>Em cartaz</Title>
+        <BannerButton>
+          <Banner 
+            source={{ uri: 'https://images.unsplash.com/photo-1593814681464-eef5af2b0628?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' }}
+          />
+        </BannerButton>
+      </ScrollView>
     </Container>
   )
 }
 
 export default Home
-// 03:00
