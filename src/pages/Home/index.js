f37194f6d83fdd68c7d30c48156be97c668c4080
AloisioFernandes <aloisio.fernandes@outlook.com>
2022-01-14 22:52:01 -0300

    Iniciando slider.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,10 +5,12 @@ import { Feather } from '@expo/vector-icons'
 import { 
   Container, 
   SearchContainer, 
+  SearchButton,
   Input, 
   Title, 
   BannerButton, 
-  Banner 
+  Banner,
+  SliderMovie
 } from './styles'
 import Header from '../../components/Header'
 
@@ -28,15 +30,21 @@ function Home() {
 
       <ScrollView>
         <Title>Em cartaz</Title>
-        <BannerButton>
-          <Banner 
+        <BannerButton activeOpacity={0.9} onPress={() => alert('TESTE')}>
+          <Banner
+            resizeMethod="resize"
             source={{ uri: 'https://images.unsplash.com/photo-1593814681464-eef5af2b0628?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' }}
           />
         </BannerButton>
+
+        <SliderMovie 
+          horizontal={true}
+          data={[1, 2, 3, 4]}
+          renderItem={({ item }) => {}}
+        />
       </ScrollView>
     </Container>
   )
 }
 
 export default Home
-// 10:30
